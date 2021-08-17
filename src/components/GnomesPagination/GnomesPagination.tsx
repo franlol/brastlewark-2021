6ef2f8b991d5e7b535e51c7d29e6b74c5006e573
franlol <fraaaaan@gmail.com>
2021-08-17 11:54:22 +0200

    feat: added pagination

---
+++ src/components/GnomesPagination/GnomesPagination.tsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { TStore } from '../../store/store';
+import { setPage } from '../../store/utils/utils.slice';
+
+import { Pagination } from '@material-ui/lab';
+import gnomesPaginationStyles from './gnomesPagination.styles';
+
+const GnomesPagination = () => {
+  const classes = gnomesPaginationStyles();
+
+  const dispatch = useDispatch();
+  const {
+    pagination: { page },
+    search: { gnomesList }
+  } = useSelector((state: TStore) => state.utils);
+
+
+  const handlePaginationChange = (_: any, page: number) => dispatch(setPage(page))
+
+  return (
+    <Pagination
+      className={classes.pagination}
+      page={page}
+      onChange={handlePaginationChange}
+      count={Math.ceil((gnomesList).length / 10)}
+      variant="outlined"
+      color="primary" />
+  )
+}
+
+export default GnomesPagination;
